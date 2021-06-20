62025ff4cfbd1925cfd1639a6ce99ada2ed7d8a7
gimminseog <gm27376@gmail.com>
2021-06-20 18:11:02 +0900

    use jsdoc

--- why-ts/app.js
+++ why-ts/app.js
@@ -9,13 +9,30 @@ var address = document.querySelector('#address');
 // user data
 var user = {};
 
-function startApp() {
-  axios
-    .get(url)
+/**
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {string} address
+ */
+
+/**
+ * @returns {Promise<User>}
+ */
+const fetchUser = () => {
+  return axios.get(url);
+}
+
+const startApp = () => {
+  fetchUser()
     .then(function (response) {
       console.log(response);
       user = response.data;
       // TODO: 이름, 이메일, 주소 표시하기
+      console.log(user);
+      username.innerText = user.name;
+      email.innerText = user.email;
+      address.innerText = user.address.city;
     })
     .catch(function (error) {
       console.log(error);
